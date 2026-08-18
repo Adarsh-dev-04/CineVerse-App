eae06899c95f4bca57b74036aaf6b0a9e27a561a
Adarsh-dev-04 <adarshagrahari9838@gmail.com>
2026-08-18 11:17:28 +0530

    fix: GMTP port

--- Backend/utils/emailService.js
+++ Backend/utils/emailService.js
@@ -2,6 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 587,
+  secure:false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
